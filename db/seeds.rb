9a7518b467d314cf96a1490e30559de8db13cb74
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-25 17:48:45 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 require_relative "../lib/student.rb"
 
-namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    Student.create_table
-  end
-end
+
 
 Student.create(name: "Melissa", grade: "10th")
 Student.create(name: "April", grade: "10th")
